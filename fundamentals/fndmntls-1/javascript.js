936df96d5dcf7042a3468bf82f8ca441ca6a7efc
jomig11 <zzorotus@gmail.com>
2022-08-09 20:28:41 +0800

    Modify javascript

--- fundamentals/fndmntls-1/javascript.js
+++ fundamentals/fndmntls-1/javascript.js
@@ -1,1 +1,21 @@
 alert("Hello World!");
+
+console.log(23 + 97);
+
+console.log(5 + 5 + 5 + 5 + 5 + 5);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+
+9 * a;
+
+let b = 7 * a;
+
+console.log(b);
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
